0cb1f6c24653626393ff9756632cfd8af67b035e
Anya Aven <leafeon96@gmail.com>
2024-07-11 20:11:45 -0400

    Added Languages and Hours charts

--- wakatime.py
+++ wakatime.py
@@ -1,13 +1,18 @@
 import requests
 import streamlit as st
 
-# Get the API key from the environment variable
+import plotly.express as px
+import pandas as pd
+
+
 api_key = st.secrets["wakatime_api_key"]
 
-# Define the endpoint you want to access
-endpoint = 'https://wakatime.com/api/v1/users/current/summaries'
 
-# Define the parameters for the request
+WAKA_TIME_URL = 'https://wakatime.com/api/v1/'
+SUMMARY_ENDPOINT = 'users/current/summaries'
+INSIGHTS_ENDPOINT = 'users/current/insights/weekdays/last_year'
+LANGUAGES_ENDPOINT = 'users/current/insights/languages/all_time'
+
 start_date = '2024-07-01'
 end_date = '2024-07-07'
 params = {
@@ -15,24 +20,85 @@ params = {
     'end': end_date
 }
 
-# Make the GET request with the API key for authentication
-response = requests.get(
-    endpoint,
+
+insights_resp = requests.get(
+    WAKA_TIME_URL + INSIGHTS_ENDPOINT,
+    headers={'Authorization': f'Basic {api_key}'})
+
+languages_response = requests.get(
+    WAKA_TIME_URL + LANGUAGES_ENDPOINT, headers={'Authorization': f'Basic {api_key}'})
+
+if insights_resp.status_code == 200:
+    data = insights_resp.json()['data']['weekdays']
+
+    st.title('WakaTime All-Time Insights')
+
+    weekdays = []
+    categories = []
+    averages = []
+    totals = []
+
+    for day in data:
+        day_name = day['name']
+        for category in day['categories']:
+            weekdays.append(day_name)
+            categories.append(category['name'])
+            # convert seconds to hours
+            averages.append(category['average'] / 3600)
+            totals.append(category['total'] / 3600)  # convert seconds to hours
+
+    df = pd.DataFrame({
+        'Weekday': weekdays,
+        'Category': categories,
+        'Average Hours': averages,
+        'Total Hours': totals
+    })
+
+    st.header('Average Hours by Category')
+    fig = px.bar(df, x='Weekday', y='Average Hours', color='Category', barmode='group',
+                 title='Average Hours by Category for Each Weekday')
+    st.plotly_chart(fig)
+
+    languages_data = languages_response.json()['data']['languages']
+    languages = []
+    total_hours = []
+
+    for language in languages_data:
+
+        languages.append(language['name'])
+        total_hours.append(language['total_seconds'] / 3600)
+
+    df_languages = pd.DataFrame({
+        'Language': languages,
+        'Total Hours': total_hours
+    })
+
+    # Display the languages data in Streamlit
+    st.header('Total Time Spent on Each Language')
+    fig = px.pie(df_languages, values='Total Hours',
+                 names='Language', title='Total Time Spent on Each Language')
+    st.plotly_chart(fig)
+
+else:
+    st.error(f'Failed to retrieve data: {
+             insights_resp.status_code} - {insights_resp.text}')
+
+summary_resp = requests.get(
+    WAKA_TIME_URL + SUMMARY_ENDPOINT,
     params=params,
     headers={'Authorization': f'Basic {api_key}'})
 
-# Check the response status
-if response.status_code == 200:
-    data = response.json()
+
+if summary_resp.status_code == 200:
+    data = summary_resp.json()
     st.title('WakaTime Data Summary')
     st.write(f"Data from {start_date} to {end_date}")
 
-    # Display the data in Streamlit
-    for summary in data['data']:
-        st.subheader(summary['range']['text'])
-        st.write(f"Total time: {summary['grand_total']['text']}")
-        for project in summary['projects']:
+    for summary_resp in data['data']:
+        st.subheader(summary_resp['range']['text'])
+        st.write(f"Total time: {summary_resp['grand_total']['text']}")
+        for project in summary_resp['projects']:
             st.write(f"Project: {project['name']}, Time: {project['text']}")
 else:
     st.error(f'Failed to retrieve data: {
-             response.status_code} - {response.text}')
+             summary_resp.status_code} - {summary_resp.text}')
